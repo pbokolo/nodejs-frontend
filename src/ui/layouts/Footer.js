bf22765f63bb0e0f027ec2c9bd73fd7e60577f12
pbokolo <dev.pbokolo@gmail.com>
2023-10-06 09:53:45 +0200

    Perfecting the copyright text

--- src/ui/layouts/Footer.js
+++ src/ui/layouts/Footer.js
@@ -2,6 +2,18 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="footer">&copy; Kitoko S.A.S.U. {Date.now()}</footer>
+    <footer className="footer">
+      <p className="text text--copyright">
+        &copy; {new Date().getFullYear()} &nbsp;
+        <a
+          href="https://github.com/pbokolo"
+          target="_blank"
+          class="text text--owner"
+        >
+          Pierrot Bokolo
+        </a>
+        &nbsp;||&nbsp;Free of use for learning but don't claim it as your design
+      </p>
+    </footer>
   );
 }
