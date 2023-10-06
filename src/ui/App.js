9a9ba4df49112fdf2371779f2e9e3cd1e1fe265a
pbokolo <dev.pbokolo@gmail.com>
2023-10-06 09:15:07 +0200

    Setting up the footer

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,10 +1,12 @@
 import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
 
 function App() {
   return (
     <main className="app">
       <Header />
       <section className="section"></section>
+      <Footer />
     </main>
   );
 }
---
+++ src/ui/layouts/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">&copy; Kitoko S.A.S.U. {Date.now()}</footer>
+  );
+}
