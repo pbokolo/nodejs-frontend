a6e8b8e923b1744cfc1214d2b54bb877a520d78b
pbokolo <dev.pbokolo@gmail.com>
2023-10-08 07:34:57 +0200

    Setting up the app logo picture and user default profile picture

--- src/ui/components/AppLogo.js
+++ src/ui/components/AppLogo.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function AppLogo() {
-  return <div className="app__logo">M</div>;
+  return (
+    <div className="app__logo">
+      <img
+        src="https://cdn.pixabay.com/photo/2015/04/18/07/49/shopping-cart-728430_1280.png"
+        alt="logo"
+        className="img img__logo"
+      />
+    </div>
+  );
 }
