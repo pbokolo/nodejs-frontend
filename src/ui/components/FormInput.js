b1ea659e036777e731d4840f232016996069ffaf
pbokolo <dev.pbokolo@gmail.com>
2023-10-09 08:00:46 +0200

    Setting up the NewStuffForm state in order to come up with controlled inputs

--- src/ui/components/FormInput.js
+++ src/ui/components/FormInput.js
@@ -1,10 +1,23 @@
 import React from "react";
 
-export default function FormInput({ label, id, placeholder, type }) {
+export default function FormInput({
+  label,
+  id,
+  placeholder,
+  type,
+  value,
+  changeHandler,
+}) {
   return (
     <fieldset>
       <label htmlFor={id}>{label}</label>
-      <input type={type} id={id} placeholder={placeholder} />
+      <input
+        type={type}
+        id={id}
+        placeholder={placeholder}
+        value={value}
+        onChange={changeHandler}
+      />
     </fieldset>
   );
 }
--- src/ui/layouts/NewStuffForm.js
+++ src/ui/layouts/NewStuffForm.js
@@ -1,29 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import FormInput from "../components/FormInput";
 
+const initialState = {
+  title: "",
+  price: "",
+  imageUrl: "",
+  description: "",
+  userId: "",
+};
 export default function NewStuffForm() {
+  const [stuff, setStuff] = useState(initialState);
+  const handleInputChange = (e) => {
+    setStuff({ ...stuff, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(stuff);
+    setStuff(initialState);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <FormInput
         label={"Titre"}
-        id="titleTxt"
+        id="title"
         placeholder={"Ex: Macbook pro"}
         type={"text"}
+        value={stuff.title}
+        changeHandler={handleInputChange}
       />
       <FormInput
         label={"Prix"}
-        id="priceTxt"
+        id="price"
         placeholder={"Ex: 30€"}
         type={"number"}
+        value={stuff.price}
+        changeHandler={handleInputChange}
       />
       <FormInput
         label={"Image"}
-        id="imgUrlTxt"
+        id="imageUrl"
         placeholder={"Ex: https://serveur/image.jpg"}
         type={"text"}
+        value={stuff.imageUrl}
+        changeHandler={handleInputChange}
       />
 
-      <textarea placeholder="Décrivez votre produit" />
+      <textarea
+        id="description"
+        placeholder="Décrivez votre produit"
+        value={stuff.description}
+        onChange={handleInputChange}
+      />
       <br />
 
       <input type="submit" value={"Vendre"} className="btn" />
