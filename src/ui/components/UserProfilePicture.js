ae47b9658c8fb7380204c43c63a3f3f349c66708
pbokolo <dev.pbokolo@gmail.com>
2023-10-12 07:13:31 +0200

    Setting up the UserProfilePicture in the Header for accessing user account

---
+++ src/ui/components/UserProfilePicture.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function UserProfilePicture() {
+  return (
+    <img
+      src="https://cdn.pixabay.com/photo/2019/01/10/12/20/animal-3925260_1280.jpg"
+      alt="utilisateur"
+      className="img img__user-profile-picture--sm"
+    />
+  );
+}
--- src/ui/layouts/Header.js
+++ src/ui/layouts/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
 import AppLogo from "../components/AppLogo";
 import NewStuffBtn from "../components/NewStuffBtn";
+import UserProfilePicture from "../components/UserProfilePicture";
 
 export default function header() {
   return (
     <header className="header">
       <AppLogo />
       <NewStuffBtn />
+      <UserProfilePicture />
     </header>
   );
 }
