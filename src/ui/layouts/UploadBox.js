9cf4f14a42724c1f4fc45c14b0ae6939dabc08ca
pbokolo <dev.pbokolo@gmail.com>
2023-10-15 19:21:53 +0200

    Listening to click event on the UploadBox

--- src/ui/layouts/UploadBox.js
+++ src/ui/layouts/UploadBox.js
@@ -1,9 +1,41 @@
 import React from "react";
 
 export default function UploadBox() {
+  let loadImage = (e) => {
+    //getting first user selected file
+    let file = e.target.files[0];
+
+    //returning if file is not selected
+    if (!file) return;
+
+    // if file selected
+
+    //passing selected file url as source to preview image
+    // previewImg.src = URL.createObjectURL(file);
+
+    //once image loaded
+    /*previewImg.addEventListener("load", () => {
+      //displaying image height & width in respective inputs
+      widthInput.value = previewImg.naturalWidth;
+      heightInput.value = previewImg.naturalHeight;
+      orgRatio = previewImg.naturalWidth / previewImg.naturalHeight;
+      wrapper.classList.add("active");
+    });*/
+  };
   return (
-    <div className="upload__box">
-      <input type="file" className="fileInput" hidden />
+    <div
+      className="upload__box"
+      onClick={(e) => {
+        document.getElementById("fileInput").click();
+      }}
+    >
+      <input
+        id="fileInput"
+        onChange={loadImage}
+        type="file"
+        className="fileInput"
+        hidden
+      />
       <img src="" alt="" className="previewImg" />
       <i className="fas fa-cloud-upload-alt"></i>
       <p>Select image from device</p>
