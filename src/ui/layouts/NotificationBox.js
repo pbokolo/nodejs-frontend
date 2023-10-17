e2ad4529d8deccd6509572c1af93ceb02e9529e8
pbokolo <dev.pbokolo@gmail.com>
2023-10-17 12:38:48 +0200

    Showing the notification text in the NotificationBox

--- src/ui/layouts/NotificationBox.js
+++ src/ui/layouts/NotificationBox.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function NotificationBox() {
+  const { text } = useSelector((state) => state.notif);
   return (
     <div className="notification">
-      <p className="text text--notification">This is a notification</p>
+      <p className="text text--notification">{text}</p>
     </div>
   );
 }
